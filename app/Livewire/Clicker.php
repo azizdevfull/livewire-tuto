cc28bcc91fbdcd2b2f7470e25d7529d8bf2ce0f0
Azizbek Isroilov <aziz16110904@gmail.com>
2024-02-29 10:25:01 +0500

    create user validation

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -4,15 +4,23 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Clicker extends Component
 {
-    public $name;
-    public $email;
-    public $password;
+    #[Rule('required|min:2|max:50')]
+    public $name = '';
+
+    #[Rule('required|email|unique:users')]
+    public $email = '';
+
+    #[Rule('required|min:5')]
+    public $password = '';
+
     public function createNewUser()
     {
+        $this->validate();
         User::create([
             'name' => $this->name,
             'email' => $this->email,
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,10 +1,48 @@
 <div>
-    <form wire:submit='createNewUser' action="">
-        <input wire:model='name' type="text" name="name">
-        <input wire:model='email' type="email" name="email">
-        <input wire:model='password' type="password" name="password">
-        <button>Create</button>
-    </form>
+    <div class="flex items-center justify-center p-12">
+        <div class="mx-auto w-full max-w-[550px]">
+            <form wire:submit='createNewUser' action="">
+                <div class="mb-5">
+                    <label for="name" class="mb-3 block text-base font-medium text-[#07074D]">
+                        Full Name
+                    </label>
+                    <input type="text" wire:model='name' name="name" id="name" placeholder="Full Name"
+                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                    @error('name')
+                        <span class="text-red-500 text-xs">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-3 block text-base font-medium text-[#07074D]">
+                        Email Address
+                    </label>
+                    <input type="email" wire:model='email' name="email" id="email"
+                        placeholder="example@domain.com"
+                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                    @error('email')
+                        <span class="text-red-500 text-xs">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="subject" class="mb-3 block text-base font-medium text-[#07074D]">
+                        Password
+                    </label>
+                    <input type="password" wire:model='password' name="password" id="subject"
+                        placeholder="Enter Password"
+                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                    @error('password')
+                        <span class="text-red-500 text-xs">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div>
+                    <button
+                        class="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none">
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
     <hr>
 
     @foreach ($users as $user)
